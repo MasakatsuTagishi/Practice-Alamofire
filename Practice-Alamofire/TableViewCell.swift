4c315956a2f3f1c9418b75e1f3451f2e5f9eed52
MasakatsuTagishi <m.t-10.22@docomo.ne.jp>
2022-06-06 14:45:28 +0900

    【追加】xibファイル追加

---
+++ Practice-Alamofire/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Practice-Alamofire
+//
+//  Created by 田岸将勝 on 2022/06/06.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
