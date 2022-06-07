38801b3c9b43ab81a5c1802b01f6608de67fa965
MasakatsuTagishi <m.t-10.22@docomo.ne.jp>
2022-06-07 12:11:29 +0900

    【追加】APIClient・APIErrorのファイルを追加 通信をしてtableViewに反映させる処理を追加

--- Practice-Alamofire/TableViewCell.swift
+++ Practice-Alamofire/TableViewCell.swift
@@ -5,26 +5,29 @@
 //  Created by 田岸将勝 on 2022/06/06.
 //
 
-import UIKit
+import SDWebImage
 
 class TableViewCell: UITableViewCell {
 
-    @IBOutlet weak var ownerNameLabel: UILabel!
-    @IBOutlet weak var ownerImageView: UIImageView!
-    @IBOutlet weak var repositoryNameLabel: UILabel!
-    @IBOutlet weak var repositoryDescriptionLabel: UILabel!
-    @IBOutlet weak var starCountLabel: UILabel!
-    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet private weak var ownerNameLabel: UILabel!
+    @IBOutlet private weak var ownerImageView: UIImageView!
+    @IBOutlet private weak var repositoryNameLabel: UILabel!
+    @IBOutlet private weak var repositoryDescriptionLabel: UILabel!
+    @IBOutlet private weak var starCountLabel: UILabel!
+    @IBOutlet private weak var languageLabel: UILabel!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configure(repository: Repository) {
+        ownerNameLabel.text = repository.name
+        repositoryNameLabel.text = repository.fullName
+        if let url = repository.owner.avatarUrl {
+            ownerImageView.sd_setImage(with: URL(string: url), completed: nil)
+        } else {
+            ownerImageView.image = nil
+        }
+        repositoryDescriptionLabel.text = repository.description ?? ""
+        starCountLabel.text = "\(repository.stargazersCount)"
+        languageLabel.text = repository.language ?? ""
+        accessoryType = .disclosureIndicator
     }
     
 }
